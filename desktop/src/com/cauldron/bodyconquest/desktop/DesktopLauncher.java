e662a587a44cb76fbea1fa54ead3c16917923969
BrandonAGoodwin <35398295+BrandonAGoodwin@users.noreply.github.com>
2019-01-20 17:49:06 +0000

    Added primative fullscreen mode functionallity using the users screen size

--- desktop/src/com/cauldron/bodyconquest/desktop/DesktopLauncher.java
+++ desktop/src/com/cauldron/bodyconquest/desktop/DesktopLauncher.java
@@ -1,5 +1,8 @@
 package com.cauldron.bodyconquest.desktop;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.cauldron.bodyconquest.rendering.Game;
@@ -7,6 +10,26 @@ import com.cauldron.bodyconquest.rendering.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = "Game";
+
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+		final int SCREEN_HEIGHT = (int) screenSize.getHeight();
+		final int SCREEN_WIDTH = (int) screenSize.getWidth();
+
+		boolean fullscreen = false;
+		if (fullscreen) {
+			config.height = SCREEN_HEIGHT;
+			config.width = SCREEN_WIDTH;
+			config.fullscreen = true;
+		} else {
+			config.height = SCREEN_HEIGHT / 2;
+			config.width = SCREEN_WIDTH / 2;
+			config.fullscreen = false;
+			config.x = SCREEN_WIDTH - config.width;
+		}
+
 		new LwjglApplication(new Game(), config);
 	}
 }
