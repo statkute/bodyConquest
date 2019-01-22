82f14d220aa79e8814c42f05633801c8cdb67a96
axm1415 <axm1415@student.bham.ac.uk>
2019-01-22 20:16:47 +0000

    added animation

--- core/src/com/cauldron/bodyconquest/Game.java
+++ core/src/com/cauldron/bodyconquest/Game.java
@@ -5,10 +5,23 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.viewport.*;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.scenes.scene2d.*;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+
+    public static Sprite backgroundSprite;
+    private Viewport gameport;
+
+    private Viewport viewport;
+    private Camera camera;
+
+    private Stage stage;
 
 	//Logger log = Logger.getLogger(Game.class);
 
@@ -16,14 +29,23 @@ public class Game extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("core/assets/badlogic.jpg");
+        backgroundSprite = new Sprite(img);
+        camera = new PerspectiveCamera();
+        viewport = new FitViewport(800, 480, camera);
+        stage = new Stage(new StretchViewport(800, 480));
 	}
+
+    public void renderBackground() {
+            backgroundSprite.draw(batch);
+    }
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // This cryptic line clears the screen.
 		batch.begin();
-		batch.draw(img, 0, 0);
+		renderBackground();
+        batch.draw(img, 0 , 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.end();
 	}
 	
@@ -32,4 +54,11 @@ public class Game extends ApplicationAdapter {
 		batch.dispose();
 		img.dispose();
 	}
+
+	@Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+        //stage.getViewport().update(width, height, true);
+    }
+
 }
--- desktop/src/com/cauldron/bodyconquest/desktop/DesktopLauncher.java
+++ desktop/src/com/cauldron/bodyconquest/desktop/DesktopLauncher.java
@@ -2,11 +2,14 @@ package com.cauldron.bodyconquest.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cauldron.bodyconquest.Animator;
 import com.cauldron.bodyconquest.Game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
+		config.resizable = false;
+        config.title = "Body Wars";
+		new LwjglApplication(new Animator(), config);
 	}
 }
