9023b33f27b6ec607ab7764c80b6272870af8182
Alexandru Nastase <axn778@cs.bham.ac.uk>
2019-01-23 12:01:00 +0000

    Added methods in the game engine

---
+++ src/com/bodyconquest/GameEngine.java
@@ -0,0 +1,38 @@
+public class GameEngine{
+
+    //Create and destroy the game
+    public void Init() {
+        //
+    }
+
+    public void Cleanup() {
+        //
+    }
+
+    // Transit between states
+    public void ChangeState(GameState state) {
+        //
+    }
+
+    public void PushState(GameState state) {
+        //
+    }   
+
+    public void PopState() {
+        //
+    }
+
+    // Actions in a game loop
+    public void HandleEvents() {
+        //
+    }
+
+    public void Update() {
+        //
+    }
+
+    public void Draw() {
+        //
+    }
+
+}
