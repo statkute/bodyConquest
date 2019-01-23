25b529fc26c90db65bdffabfb51bd56f9c682712
Alexandru Nastase <axn778@cs.bham.ac.uk>
2019-01-23 12:03:33 +0000

    Added GameState file and methods

---
+++ src/com/bodyconquest/GameState.java
@@ -0,0 +1,31 @@
+public class GameState{
+
+    // Setup and destroy the state
+    public void Init(){
+        //
+    }
+    public void Cleanup(){
+        //
+    }
+
+    // Used when temporarily transitioning to another state
+    public void Pause(){
+        //
+    }
+    public void Resume(){
+        //
+    }
+
+    // The three important actions within a game loop
+    public void HandleEvents(){
+        //
+    }
+    public void Update(){
+        //
+    }
+    public void Draw(){
+        //
+    }
+
+
+}
