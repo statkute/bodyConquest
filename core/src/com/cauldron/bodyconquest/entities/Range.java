8b9807f52610c7adad27f7923bc417d8088baa1e
axm1415 <axm1415@student.bham.ac.uk>
2019-01-24 14:47:42 +0000

    range and soldier added

---
+++ core/src/com/cauldron/bodyconquest/entities/Range.java
@@ -0,0 +1,11 @@
+package com.cauldron.bodyconquest.entities;
+
+public class Range extends Entity {
+
+
+    public Range(String type, int health) {
+        super(type, health);
+    }
+
+
+}
---
+++ core/src/com/cauldron/bodyconquest/entities/Soldier.java
@@ -0,0 +1,9 @@
+package com.cauldron.bodyconquest.entities;
+
+public class Soldier extends Entity {
+
+
+    public Soldier(String type, int health) {
+        super(type, health);
+    }
+}
