31fe78ba16e6b748904cab8ab33f254f387962d5
Gintare <statkute.g@gmail.com>
2019-01-29 17:21:12 +0000

    Added troops
    Former-commit-id: 5edb1664df536e416864f7a7ff09eb0767dd1103

---
+++ core/src/com/cauldron/bodyconquest/enities/Troops/Archer.java
@@ -0,0 +1,7 @@
+package com.cauldron.bodyconquest.enities.Troops;
+
+public class Archer extends Troop {
+  public Archer() {
+    super(10.0, 2, 4.7, 1.6, 18.2, 23.8, 18.3, 55.0);
+  }
+}
---
+++ core/src/com/cauldron/bodyconquest/enities/Troops/Troop.java
@@ -0,0 +1,73 @@
+package com.cauldron.bodyconquest.enities.Troops;
+
+public abstract class Troop {
+  private double damagePerHit;
+  private int range;
+  private double movementSpeed;
+  private double attackSpeed;
+  private double lipidsCost;
+  private double sugarsCost;
+  private double proteinCost;
+  private double health;
+
+  public Troop() {}
+
+  public Troop(
+      double damagePerHit,
+      int range,
+      double movementSpeed,
+      double attackSpeed,
+      double lipidsCost,
+      double sugarsCost,
+      double proteinCost,
+      double health) {
+    this.damagePerHit = damagePerHit;
+    this.range = range;
+    this.movementSpeed = movementSpeed;
+    this.attackSpeed = attackSpeed;
+    this.lipidsCost = lipidsCost;
+    this.sugarsCost = sugarsCost;
+    this.proteinCost = proteinCost;
+    this.health = health;
+  }
+
+  public void dealDamage(double damage) {
+    if (damage >= health) {
+      health = 0.0;
+    } else {
+      health -= damage;
+    }
+  }
+
+  public double getAttackSpeed() {
+    return attackSpeed;
+  }
+
+  public double getDamagePerHit() {
+    return damagePerHit;
+  }
+
+  public double getHealth() {
+    return health;
+  }
+
+  public double getLipidsCost() {
+    return lipidsCost;
+  }
+
+  public double getMovementSpeed() {
+    return movementSpeed;
+  }
+
+  public double getProteinCost() {
+    return proteinCost;
+  }
+
+  public double getSugarsCost() {
+    return sugarsCost;
+  }
+
+  public int getRange() {
+    return range;
+  }
+}
