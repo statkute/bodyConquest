ed268be646250af80eae7a7a7c2dfe34271b17e0
Gintare <statkute.g@gmail.com>
2019-02-16 11:17:54 +0000

    Added EncounterStateUpdator
    Former-commit-id: 4e2aeb48693b77f568daf88697ae924e3e15fa0b

--- core/src/com/cauldron/bodyconquest/networking/EncounterStateUpdator.java
+++ core/src/com/cauldron/bodyconquest/networking/EncounterStateUpdator.java
@@ -1,16 +1,23 @@
 package com.cauldron.bodyconquest.networking;
 
 import com.cauldron.bodyconquest.game_logic.utils.Timer;
+import com.cauldron.bodyconquest.networking.utilities.Serialization;
 import com.cauldron.bodyconquest.screens.RaceSelection;
 
-public class EncounterStateUpdator extends Thread{
-  public EncounterStateUpdator(){
-
+public class EncounterStateUpdator extends Thread {
+  private ServerSender serverSender;
+  public EncounterStateUpdator(ServerSender serverSender) {
+    this.serverSender = serverSender;
   }
 
-  public void run(){
-    while (true){
-
+  public void run() {
+    while (true) {
+      // get the encounterState
+      // String json = Serialization.serialize(encounterState);
+      // Message message = "ENCOUNTERSTATE_UPDATE_"
+      // message += json
+      // serverSender.sendMessage(message)
+      
       Timer.startTimer(600);
     }
   }
