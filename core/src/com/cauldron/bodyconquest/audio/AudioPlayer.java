5e1bfb7b5b2f60361e6279e89bd4b9ca32170372
BrandonAGoodwin <35398295+BrandonAGoodwin@users.noreply.github.com>
2019-02-19 17:46:52 +0000

    Partially javadoc'd AudioPlayer class and need to change audio in HostScreen
    Former-commit-id: b4281610baf0c9df9d3156759accfd9d0ee1b71c

--- core/src/com/cauldron/bodyconquest/audio/AudioPlayer.java
+++ core/src/com/cauldron/bodyconquest/audio/AudioPlayer.java
@@ -6,24 +6,36 @@ import com.badlogic.gdx.audio.Sound;
 
 import java.util.HashMap;
 
-/* Controls audio within the game*/
+/**
+ *  Controls audio within the game
+ */
 public class AudioPlayer {
 
+  /** The the max volume for all sounds inside the game. (Initialised to {@value MASTER_VOLUME}) */
   public static float MASTER_VOLUME = 1.0f;
+  /** The volume for all sound FX inside the game. (Initialised to {@value SFX_VOLUME}) */
   public static float SFX_VOLUME = 1.0f;
+  /** The volume for all the music inside the game. (Initialised to {@value MUSIC_VOLUME}) */
   public static float MUSIC_VOLUME = 1.0f;
 
+  /** The volume to set for muted sounds. */
   private final float MUTED_VOLUME = 0.0f;
 
+  /** The HashMap that stores all the loaded sound FX. */
   private HashMap<String, Sound> soundFX;
+  /** The HashMap that stored all the loaded music. */
   private HashMap<String, Music> music;
 
+  /** Stores the music that is currently playing. (Null if no music has been set to play yet) */
   private Music currentMusic;
 
   private boolean mutedSFX;
   private boolean mutedMusic;
   private boolean muted;
 
+  /**
+   * Constructor.
+   */
   public AudioPlayer(){
     soundFX = new HashMap<String, Sound>();
     music = new HashMap<String, Music>();
@@ -35,8 +47,10 @@ public class AudioPlayer {
     currentMusic = null;
   }
 
+  /**
+   * Toggle if all sounds are muted.
+   */
   public void toggleMuted() {
-
     if(muted) {
       muted = false;
       if(currentMusic != null) currentMusic.setVolume(MUSIC_VOLUME);
@@ -46,6 +60,9 @@ public class AudioPlayer {
     }
   }
 
+  /**
+   * Toggle if sound FX are muted.
+   */
   public void toggleMutedSFX() {
     if(mutedSFX) {
       mutedSFX = false;
@@ -54,6 +71,9 @@ public class AudioPlayer {
     }
   }
 
+  /**
+   * Toggle if music is muted.
+   */
   public void toggleMutedMusic() {
     if(mutedMusic) {
       mutedMusic = false;
@@ -64,6 +84,11 @@ public class AudioPlayer {
     }
   }
 
+  /**
+   * Load in a new sound effect.
+   * @param name The name used to play this sound.
+   * @param path The path to location of the sound file.
+   */
   public void loadSFX(String name, String path) {
     soundFX.put(name, Gdx.audio.newSound(Gdx.files.internal(path)));
   }
@@ -77,6 +102,11 @@ public class AudioPlayer {
     music.put(name, Gdx.audio.newMusic(Gdx.files.internal(path)));
   }
 
+  /**
+   * Play the sound effect that was loaded in with the same name.
+   * Plays the sound silently if the all the sounds or SFX have been muted.
+   * @param name The sound to play.
+   */
   public void playSFX(String name) {
     if(muted) {
       soundFX.get(name).play(MUTED_VOLUME * MASTER_VOLUME);
@@ -85,14 +115,30 @@ public class AudioPlayer {
     }
   }
 
+  /**
+   * Play the music that was loaded in with the same name once.
+   * Plays the music silently if all the sounds or music have been muted.
+   * @param name The music to play.
+   */
   public void playMusicOnce(String name) {
     playMusic(name, false);
   }
 
+  /**
+   * Play the music that was loaded in with the same name on a constant loop.
+   * Plays the music silently if all the sounds or music have been muted.
+   * @param name The music to play.
+   */
   public void playMusicLoop(String name) {
     playMusic(name, true);
   }
 
+  /**
+   * Play the music that was loaded in with the same name, specifying whether the music should be played on a loop.
+   * Plays the music silently if all the sounds or music have been muted.
+   * @param name
+   * @param loop
+   */
   public void playMusic(String name, boolean loop) {
     if(currentMusic != null) currentMusic.stop();
     currentMusic = music.get(name);
