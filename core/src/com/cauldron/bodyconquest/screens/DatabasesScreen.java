51ec21144abfdc89068b9dcaafa45e338e43e9d8
AugustasMacys <augustasmacys@gmail.com>
2019-03-07 18:32:39 +0000

    Started on refactoring and login screen as registering and login Screen are almost identical
    Former-commit-id: 3827253e1d4c8dadb0dd238e87874e329bef7570

---
+++ core/src/com/cauldron/bodyconquest/screens/DatabasesScreen.java
@@ -0,0 +1,9 @@
+package com.cauldron.bodyconquest.screens;
+
+import com.cauldron.bodyconquest.rendering.BodyConquest;
+
+public abstract class DatabasesScreen extends AbstractGameScreen {
+    public DatabasesScreen(BodyConquest game) {
+        super(game);
+    }
+}
---
+++ core/src/com/cauldron/bodyconquest/screens/LoginScreen.java
@@ -0,0 +1,13 @@
+package com.cauldron.bodyconquest.screens;
+
+import com.badlogic.gdx.Screen;
+import com.cauldron.bodyconquest.rendering.BodyConquest;
+
+public class LoginScreen extends DatabasesScreen implements Screen {
+
+
+
+    public LoginScreen(BodyConquest game) {
+        super(game);
+    }
+}
