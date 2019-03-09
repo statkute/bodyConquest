7cb5d23438a87f9fc343c0413620c10af07ed5bb
Gintare <statkute.g@gmail.com>
2019-03-09 21:45:16 +0000

    StartGame window visuals changed (will need to be altered when merged with RenderingNew/Abilities branch
    Former-commit-id: 4571263f276dbfff8038105325f099a6522460f7

--- core/src/com/cauldron/bodyconquest/screens/StartScreen.java
+++ core/src/com/cauldron/bodyconquest/screens/StartScreen.java
@@ -9,81 +9,115 @@ import com.cauldron.bodyconquest.rendering.BodyConquest;
 
 public class StartScreen extends AbstractGameScreen implements Screen {
 
-    private Texture title;
-    private Texture register;
-    private Texture login;
-    private Rectangle registerBounds;
-    private Rectangle loginBounds;
+  private Texture title;
+  private Texture register;
+  private Texture login;
+  private Texture exitButton;
+  private Rectangle registerBounds;
+  private Rectangle loginBounds;
+  private Rectangle exitBounds;
 
-    //MyTextInputListener listener = new MyTextInputListener();
-    //Gdx.input.getTextInput(listener, "Dialog Title", "Initial Textfield Value", "Hint Value");
+  // MyTextInputListener listener = new MyTextInputListener();
+  // Gdx.input.getTextInput(listener, "Dialog Title", "Initial Textfield Value", "Hint Value");
 
-    public StartScreen(BodyConquest game) {
-        super(game);
-        loadAssets();
-        getAssets();
-        setRectangles();
-    }
+  public StartScreen(BodyConquest game) {
+    super(game);
+    loadAssets();
+    getAssets();
+    setRectangles();
+  }
 
-    @Override
-    public void render(float delta) {
-        super.render(delta);
-        game.batch.begin();
-        game.batch.draw(background, 0, 0, BodyConquest.V_WIDTH, BodyConquest.V_HEIGHT);
-        game.batch.draw(title,BodyConquest.V_WIDTH / 2.0f - title.getWidth() / 2.0f, 450);
-        game.batch.draw(register,BodyConquest.V_WIDTH / 2.0f - register.getWidth() / 3.0f, 300,register.getWidth()/1.5f,register.getHeight()/1.50f);
-        game.batch.draw(login,BodyConquest.V_WIDTH / 2.0f - login.getWidth() / 3.0f, 200,login.getWidth()/1.5f,login.getHeight()/1.50f);
-        checkPressed();
-        game.batch.end();
-    }
+  @Override
+  public void render(float delta) {
+    super.render(delta);
+    game.batch.begin();
+    game.batch.draw(background, 0, 0, BodyConquest.V_WIDTH, BodyConquest.V_HEIGHT);
+    game.batch.draw(title, BodyConquest.V_WIDTH / 2.0f - title.getWidth() / 2.0f, 450);
+    game.batch.draw(
+        login,
+        BodyConquest.V_WIDTH / 2.0f - login.getWidth() / 1.77f / 2.0f,
+        300,
+        login.getWidth() / 1.77f,
+        login.getHeight() / 1.77f);
+    game.batch.draw(
+        register,
+        BodyConquest.V_WIDTH / 2.0f - register.getWidth() / 1.77f / 2.0f,
+        240,
+        register.getWidth() / 1.77f,
+        register.getHeight() / 1.77f);
+    game.batch.draw(exitButton, BodyConquest.V_WIDTH / 2 - exitButton.getWidth() / 2, 60);
+    checkPressed();
+    game.batch.end();
+  }
 
-    @Override
-    public void checkPressed() {
-        super.checkPressed();
+  @Override
+  public void checkPressed() {
+    super.checkPressed();
 
-        if (Gdx.input.justTouched()) {
+    if (Gdx.input.justTouched()) {
 
-            if(loginBounds.contains(tmp.x,tmp.y)){
-                playButtonSound();
-                //System.out.println("Login touched");
-                dispose();
-                game.setScreen(new LoginScreen(game));
-            }
+      if (loginBounds.contains(tmp.x, tmp.y)) {
+        playButtonSound();
+        // System.out.println("Login touched");
+        dispose();
+        game.setScreen(new LoginScreen(game));
+      }
 
-            if(registerBounds.contains(tmp.x,tmp.y)){
-                playButtonSound();
-                //System.out.println("register touched");
-                dispose();
-                game.setScreen(new RegisteringScreen(game));
-            }
+      if (registerBounds.contains(tmp.x, tmp.y)) {
+        playButtonSound();
+        // System.out.println("register touched");
+        dispose();
+        game.setScreen(new RegisteringScreen(game));
+      }
 
+        if (exitBounds.contains(tmp.x, tmp.y)) {
+            playButtonSound();
+            dispose();
+            Gdx.app.exit();
+            System.exit(0);
         }
-
-
-
     }
+  }
 
-    @Override
-    public void loadAssets() {
-        super.loadAssets();
-        manager.load(Assets.startTitle, Texture.class);
-        manager.load(Assets.startLogin, Texture.class);
-        manager.load(Assets.startRegister, Texture.class);
-        manager.finishLoading();
-    }
+  @Override
+  public void loadAssets() {
+    super.loadAssets();
+    manager.load(Assets.startTitle, Texture.class);
+    manager.load(Assets.startLogin, Texture.class);
+    manager.load(Assets.startRegister, Texture.class);
+    manager.load(Assets.exitButton, Texture.class);
+    manager.finishLoading();
+  }
 
-    @Override
-    public void getAssets() {
-        super.getAssets();
-        title = manager.get(Assets.startTitle, Texture.class);
-        login = manager.get(Assets.startLogin, Texture.class);
-        register = manager.get(Assets.startRegister, Texture.class);
-    }
+  @Override
+  public void getAssets() {
+    super.getAssets();
+    title = manager.get(Assets.startTitle, Texture.class);
+    login = manager.get(Assets.startLogin, Texture.class);
+    register = manager.get(Assets.startRegister, Texture.class);
+    exitButton = manager.get(Assets.exitButton, Texture.class);
+  }
 
-    @Override
-    public void setRectangles() {
-        super.setRectangles();
-        loginBounds = new Rectangle(BodyConquest.V_WIDTH / 2.0f - login.getWidth() / 3.0f, 200,login.getWidth()/1.5f,login.getHeight()/1.50f);
-        registerBounds = new Rectangle(BodyConquest.V_WIDTH / 2.0f - register.getWidth() / 3.0f, 300,register.getWidth()/1.5f,register.getHeight()/1.50f);
-    }
+  @Override
+  public void setRectangles() {
+    super.setRectangles();
+    loginBounds =
+        new Rectangle(
+            BodyConquest.V_WIDTH / 2.0f - login.getWidth() / 1.77f / 2.0f,
+            300,
+            login.getWidth() / 1.77f,
+            login.getHeight() / 1.77f);
+    registerBounds =
+        new Rectangle(
+            BodyConquest.V_WIDTH / 2.0f - register.getWidth() / 1.77f / 2.0f,
+            240,
+            register.getWidth() / 1.77f,
+            register.getHeight() / 1.77f);
+    exitBounds =
+        new Rectangle(
+            BodyConquest.V_WIDTH / 2 - exitButton.getWidth() / 2,
+            60,
+            exitButton.getWidth(),
+            exitButton.getHeight());
+  }
 }
