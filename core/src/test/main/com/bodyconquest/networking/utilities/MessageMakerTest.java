d5f063d62c06b3f499b496217c16eaec74b17ab8
Gintare <statkute.g@gmail.com>
2019-03-16 20:23:36 +0000

    MessageMaker testing complete
    Former-commit-id: 87838a55e63ad62dc798c62ad6663f172a8dd6ae

--- core/src/test/main/com/bodyconquest/networking/utilities/MessageMakerTest.java
+++ core/src/test/main/com/bodyconquest/networking/utilities/MessageMakerTest.java
@@ -2,6 +2,8 @@ package main.com.bodyconquest.networking.utilities;
 
 import main.com.bodyconquest.constants.AbilityType;
 import main.com.bodyconquest.constants.Assets;
+import main.com.bodyconquest.constants.Disease;
+import main.com.bodyconquest.constants.Organ;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -54,17 +56,80 @@ public class MessageMakerTest {
   }
 
   @Test
-  public void resourceUpdate() {}
+  public void resourceUpdate() {
+    String target1 = MessageMaker.resourceUpdate(100, 100, 100, Assets.PlayerType.PLAYER_BOTTOM);
+    assertEquals("RESOURCES_PB_100_100_100", target1);
+
+    String target2 = MessageMaker.resourceUpdate(0, 3, 19, Assets.PlayerType.PLAYER_TOP);
+    assertEquals("RESOURCES_PT_000_003_019", target2);
+
+    String target3 = MessageMaker.resourceUpdate(30, 87, 0, Assets.PlayerType.PLAYER_TOP);
+    assertEquals("RESOURCES_PT_030_087_000", target3);
+  }
 
   @Test
-  public void diseaseMessage() {}
+  public void diseaseMessage() {
+    String target1 = MessageMaker.diseaseMessage(Disease.INFLUENZA, Assets.PlayerType.PLAYER_TOP);
+    assertEquals("RACE_FLU_PT", target1);
+
+    String target2 = MessageMaker.diseaseMessage(Disease.ROTAVIRUS, Assets.PlayerType.PLAYER_BOTTOM);
+    assertEquals("RACE_RVI_PB", target2);
+
+    String target3 = MessageMaker.diseaseMessage(Disease.MEASLES, Assets.PlayerType.PLAYER_BOTTOM);
+    assertEquals("RACE_MES_PB", target3);
+
+    String target4 = MessageMaker.diseaseMessage(Disease.ROTAVIRUS, Assets.PlayerType.PLAYER_TOP);
+    assertEquals("RACE_RVI_PT", target4);
+  }
 
   @Test
-  public void confirmRaceMessage() {}
+  public void confirmRaceMessage() {
+    String target1 = MessageMaker.confirmRaceMessage(Assets.PlayerType.PLAYER_TOP);
+    assertEquals("CONFIRM_RACE_PT", target1);
+
+    String target2 = MessageMaker.confirmRaceMessage(Assets.PlayerType.PLAYER_BOTTOM);
+    assertEquals("CONFIRM_RACE_PB", target2);
+  }
 
   @Test
-  public void confirmOrganMessage() {}
+  public void confirmOrganMessage() {
+    String target1 = MessageMaker.confirmOrganMessage(Organ.BRAIN);
+    assertEquals("CONFIRM_ORGAN_BRA", target1);
+
+    String target2 = MessageMaker.confirmOrganMessage(Organ.EYES);
+    assertEquals("CONFIRM_ORGAN_EYE", target2);
+
+    String target3 = MessageMaker.confirmOrganMessage(Organ.HEART);
+    assertEquals("CONFIRM_ORGAN_HRT", target3);
+
+    String target4 = MessageMaker.confirmOrganMessage(Organ.LUNGS);
+    assertEquals("CONFIRM_ORGAN_LNG", target4);
+
+    String target5 = MessageMaker.confirmOrganMessage(Organ.TEETH);
+    assertEquals("CONFIRM_ORGAN_TEE", target5);
+
+    String target6 = MessageMaker.confirmOrganMessage(Organ.INTESTINES);
+    assertEquals("CONFIRM_ORGAN_INT", target6);
+  }
 
   @Test
-  public void startEncounterMessage() {}
+  public void startEncounterMessage() {
+    String target1 = MessageMaker.startEncounterMessage(Organ.BRAIN);
+    assertEquals("START_ENCOUNTER_BRA", target1);
+
+    String target2 = MessageMaker.startEncounterMessage(Organ.EYES);
+    assertEquals("START_ENCOUNTER_EYE", target2);
+
+    String target3 = MessageMaker.startEncounterMessage(Organ.HEART);
+    assertEquals("START_ENCOUNTER_HRT", target3);
+
+    String target4 = MessageMaker.startEncounterMessage(Organ.LUNGS);
+    assertEquals("START_ENCOUNTER_LNG", target4);
+
+    String target5 = MessageMaker.startEncounterMessage(Organ.TEETH);
+    assertEquals("START_ENCOUNTER_TEE", target5);
+
+    String target6 = MessageMaker.startEncounterMessage(Organ.INTESTINES);
+    assertEquals("START_ENCOUNTER_INT", target6);
+  }
 }
