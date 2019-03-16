5f46de45f3d6c57db27534195be308b0fc38e8e8
Alexandru Nastase <axn778@cs.bham.ac.uk>
2019-03-16 17:35:19 +0000

    added tests framework for database
    Former-commit-id: fc623f04bc10b66e9ca0caa034ed7ec65fcdaba2

---
+++ core/src/test/main/com/bodyconquest/database/DatabaseManagerTest.java
@@ -0,0 +1,24 @@
+package main.com.bodyconquest.database;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DatabaseManagerTest {
+
+    @Test
+    public void connect() {
+    }
+
+    @Test
+    public void addUser() {
+    }
+
+    @Test
+    public void checkUser() {
+    }
+
+    @Test
+    public void getUserAchievements() {
+    }
+}
