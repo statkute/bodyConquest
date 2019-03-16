47265495a3b2a3fbcd7d97b5d6597ba1a33aed9d
Alexandru Nastase <axn778@cs.bham.ac.uk>
2019-03-16 18:02:23 +0000

    added tests for projectile
    Former-commit-id: 10e48d53c894a22040df82bee43422568e70a4d6

---
+++ core/src/test/main/com/bodyconquest/entities/projectiles/FluProjectileTest.java
@@ -0,0 +1,43 @@
+package main.com.bodyconquest.entities.projectiles;
+
+import main.com.bodyconquest.entities.Troops.Bacteria;
+import main.com.bodyconquest.entities.Troops.Troop;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FluProjectileTest {
+    FluProjectile fp;
+
+    @Before
+    public void init(){
+        fp = new FluProjectile(10,10,10,20,20);
+    }
+
+    @Test
+    public void update() {
+        fp.update();
+        assertFalse(fp.getRemove());
+    }
+
+    @Test
+    public void setRemoved(){
+        fp.setRemove();
+        assertTrue(fp.remove);
+    }
+
+    @Test
+    public void getRemove(){
+        fp.setRemove();
+        assertTrue(fp.getRemove());
+    }
+
+    @Test
+    public void hit(){
+        Troop t = new Bacteria();
+        fp.hit(t);
+        assertTrue(fp.getRemove());
+        assertEquals(t.getHealth(), t.getMaxHealth()-10);
+    }
+}
