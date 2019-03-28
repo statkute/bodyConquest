fa1f263cbbc65da8fe99c93abad850b21cab44f0
Paulos <pxt708@student.bham.ac.uk>
2019-03-28 18:22:16 +0000

    finished javadocs
    Former-commit-id: 706194919b91ff2180dbec3d4c70c9ad168ccf49

--- core/src/main/com/bodyconquest/entities/Map.java
+++ core/src/main/com/bodyconquest/entities/Map.java
@@ -1,6 +1,5 @@
 package main.com.bodyconquest.entities;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -13,97 +12,94 @@ import main.com.bodyconquest.handlers.AnimationWrapper;
  */
 public class Map extends Actor {
 
-  private final double MAP_HEIGHT = 516.0;
-  private final double MAP_WIDTH = 516.0;
+    private final double MAP_WIDTH = 516.0;
 
-  private float stateTime;
-  private int frameCols;
-  private int frameRows;
-  private float frameRate;
-  private Animation<TextureRegion> walkAnimation;
-  private int points;
+    private float stateTime;
+    private int frameCols;
+    private int frameRows;
+    private float frameRate;
+    private Animation<TextureRegion> walkAnimation;
+    private int points;
 
-  /**
-   * Instantiates a new Map.
-   *
-   * @param organ the organ whose map is to be instantiated
-   */
+    /**
+     * Instantiates a new Map.
+     *
+     * @param organ the organ whose map is to be instantiated
+     */
 // Should have some sort of resource manager and system
-  public Map(Organ organ) {
-    String texturePath = null;
-    if(organ == Organ.LUNGS){
-      texturePath = "core/assets/map_lungs_ss.png";
-      frameCols = 4;
-      frameRows = 5;
-      frameRate = 30f;
-      points =30;
-    }
-    if(organ == Organ.EYES)  {
-      texturePath = "core/assets/map_eyes_ss.png";
-      frameCols = 2;
-      frameRows = 11;
-      frameRate = 60f;
-      points =20;
-    }
-    if(organ == Organ.HEART)  {
-      texturePath = "core/assets/map_heart_ss.png";
-      frameCols = 11;
-      frameRows = 1;
-      frameRate = 40f;
-      points = 30;
-    }
-    if(organ == Organ.TEETH)  {
-      texturePath = "core/assets/map_teeth_ss.png";
-      frameCols = 5;
-      frameRows = 5;
-      frameRate = 47f;
-      points = 10;
-    }
-    if(organ == Organ.BRAIN)  {
-      texturePath = "core/assets/map_brain_ss.png";
-      frameCols = 4;
-      frameRows = 5;
-      frameRate = 60f;
-      points = 40;
+    public Map(Organ organ) {
+        String texturePath = null;
+        if (organ == Organ.LUNGS) {
+            texturePath = "core/assets/map_lungs_ss.png";
+            frameCols = 4;
+            frameRows = 5;
+            frameRate = 30f;
+            points = 30;
+        }
+        if (organ == Organ.EYES) {
+            texturePath = "core/assets/map_eyes_ss.png";
+            frameCols = 2;
+            frameRows = 11;
+            frameRate = 60f;
+            points = 20;
+        }
+        if (organ == Organ.HEART) {
+            texturePath = "core/assets/map_heart_ss.png";
+            frameCols = 11;
+            frameRows = 1;
+            frameRate = 40f;
+            points = 30;
+        }
+        if (organ == Organ.TEETH) {
+            texturePath = "core/assets/map_teeth_ss.png";
+            frameCols = 5;
+            frameRows = 5;
+            frameRate = 47f;
+            points = 10;
+        }
+        if (organ == Organ.BRAIN) {
+            texturePath = "core/assets/map_brain_ss.png";
+            frameCols = 4;
+            frameRows = 5;
+            frameRate = 60f;
+            points = 40;
+        }
+
+        if (organ == Organ.INTESTINES) {
+            texturePath = "core/assets/map_brain_ss.png";
+            frameCols = 4;
+            frameRows = 5;
+            frameRate = 60f;
+            points = 20;
+        }
+        //animation = GifDecoder.loadGIFAnimation(Animation.PlayMode.LOOP, Gdx.files.internal(texturePath).read());
+        walkAnimation = AnimationWrapper.getSpriteSheet(frameCols, frameRows, frameRate, texturePath);
+        stateTime = 0f;
     }
 
-    if(organ == Organ.INTESTINES)  {
-      texturePath = "core/assets/map_brain_ss.png";
-      frameCols = 4;
-      frameRows = 5;
-      frameRate = 60f;
-      points = 20;
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        stateTime += 10f; // Accumulate elapsed animation time
+        // Get current frame of animation for the current stateTime
+
+        TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+
+        batch.draw(currentFrame, getX(), getY(), getWidth(), getHeight());
+        super.draw(batch, parentAlpha);
     }
-    //animation = GifDecoder.loadGIFAnimation(Animation.PlayMode.LOOP, Gdx.files.internal(texturePath).read());
-    walkAnimation = AnimationWrapper.getSpriteSheet(frameCols, frameRows, frameRate, texturePath);
-    stateTime = 0f;
-  }
 
-  @Override
-  public void draw(Batch batch, float parentAlpha) {
-//    stateTime += Gdx.graphics.getDeltaTime();
-//    TextureRegion currentFrame = animation.getKeyFrame(stateTime, true);
-    stateTime += 10f; // Accumulate elapsed animation time
-    // Get current frame of animation for the current stateTime
+    public float getRight() {
+        return (float) MAP_WIDTH;
+    }
 
-    TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
-
-    batch.draw(currentFrame, getX(), getY(), getWidth(), getHeight());
-    super.draw(batch, parentAlpha);
-  }
-
-  public float getRight() {
-    return (float) MAP_WIDTH;
-  }
-
-  /**
-   * Get points.
-   *
-   * @return the points
-   */
-  public int getPoints(){
-    return points;
-  }
+    /**
+     * Get points.
+     *
+     * @return the points
+     */
+    public int getPoints() {
+        return points;
+    }
 
   /*
   Here should be a function to set which map image this should have and
--- core/src/main/com/bodyconquest/entities/Spawnable.java
+++ core/src/main/com/bodyconquest/entities/Spawnable.java
@@ -1,10 +1,35 @@
 package main.com.bodyconquest.entities;
 
+/**
+ * The interface Spawnable.
+ */
 public interface Spawnable {
 
-  int getSugarCost();
-  int getLipidCost();
-  int getProteinCost();
+    /**
+     * Gets sugar cost.
+     *
+     * @return the sugar cost
+     */
+    int getSugarCost();
 
-  String getPortraitLocation();
+    /**
+     * Gets lipid cost.
+     *
+     * @return the lipid cost
+     */
+    int getLipidCost();
+
+    /**
+     * Gets protein cost.
+     *
+     * @return the protein cost
+     */
+    int getProteinCost();
+
+    /**
+     * Gets portrait location.
+     *
+     * @return the portrait location
+     */
+    String getPortraitLocation();
 }
